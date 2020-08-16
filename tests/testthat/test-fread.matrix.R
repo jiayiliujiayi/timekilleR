9de8ceea7ac5f71dde1cd15a3e57d1dfaf915d0f
Jiayi Liu <jiayiliujiayiliu@gmail.com>
2020-08-16 03:48:02 -0500

    fixed test: matrix class equals to matrix and array

--- tests/testthat/test-fread.matrix.R
+++ tests/testthat/test-fread.matrix.R
@@ -1,5 +1,5 @@
 test_that("fread.matrix returns a matrix", {
   x = fread.matrix('https://raw.githubusercontent.com/jiayiliujiayi/timekiller/master/testdata/dataset_matrix.txt')
   expect_equal(mode(x), "numeric")
-  expect_equal(class(x), "matrix")
+  expect_equal(class(x) %>% sort(), c("matrix", "array") %>% sort)
 })
